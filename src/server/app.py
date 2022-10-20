055fba8865b0c6a24735b20af512715fdbd215cd
Michael Mason <TeaUponTweed@gmail.com>
2022-10-19 23:29:04 -0600

    initial server commit

---
+++ src/server/app.py
@@ -0,0 +1,35 @@
+import os
+import pathlib
+import random
+import logging
+
+from flask import (
+    Flask,
+    send_file,
+    send_from_directory,
+    jsonify,
+)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return send_file("static/index.html")
+
+
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory("static", path)
+
+@app.route("/text")
+def text():
+    return jsonify(
+        {
+            "words": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".split(),
+            # "words": "zoop zoop".split(),
+        }
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
